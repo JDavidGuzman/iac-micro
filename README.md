052542694d16f551eef86df7a3548ca4749bc3e3
David Guzmám <>
2021-05-07 09:59:42 -0500

    Infrastructure as code for kubernetes master node setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# kubernetes-ansible-vagrant
+# Kubernetes, Ansible and Vagrant
+
+Here is infrastructure as code in order to setup a kubernetes cluster on vagrant machines with virtual box provider behind a nginx load balancer using docker and ansible
---
+++ vagrant/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Generate ssh keys"
+./keys/create_keys.sh keys
+echo "Create Linux VM"
+vagrant up
+echo "Create nginx Load Balancer"
+docker-compose -f ../ansible/docker-compose.yml run --rm -d --service-ports nginx_lb
+echo "Initiate provisioning with Ansible"
+docker-compose -f ../ansible/docker-compose.yml run --rm ansible ansible-playbook -i inventory.yml playbook.yml
